05d97782718e9c79615249f9125886326fe2954a
Clément Battard <c.battard@ldlc.com>
2016-01-20 21:45:40 +0100

    commit

---
+++ src/main/java/com/iut/dasi/TestRest.java
@@ -0,0 +1,20 @@
+package com.iut.dasi;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Component;
+
+@Component
+
+@Path("/helloworld")
+public class TestRest {
+	
+	@GET
+	 @Path("/toto")
+	 
+	 public String simplejson() {
+		 return "Yes";
+	 }
+
+}
